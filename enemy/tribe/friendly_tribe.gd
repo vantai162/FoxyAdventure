b4a1d132a90e0b5bcb9dd93ca7adf49b06c43cbe
Hoàng Văn Tài <hoangvantai162@gmail.com>
2025-11-12 15:23:10 +0700

    chinh lai friedly_tribe

--- enemy/tribe/friendly_tribe.gd
+++ enemy/tribe/friendly_tribe.gd
@@ -3,3 +3,11 @@ extends EnemyCharacter
 func _ready() -> void:
 	fsm = FSM.new(self, $States, $States/Run)
 	super._ready()
+
+func _on_player_in_sight(_player_pos: Vector2) -> void:
+	if fsm.current_state.name != "surprise" and fsm.current_state.name != "flee":
+		fsm.change_state(fsm.states.surprise)
+
+func _on_player_not_in_sight() -> void:
+	if fsm.current_state.name == "flee" or fsm.current_state.name == "surprise":
+		fsm.change_state(fsm.states.run)
