454af788f404a41fb48119c97279a5751edf9efa
Hoàng Văn Tài <hoangvantai162@gmail.com>
2025-11-14 23:11:21 +0700

    them one way platform, refactor sound asset, them transition screen, chinh hurt sound cho vai enemy

---
+++ scenes/transition_screen.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+signal on_transition_finished
+
+@onready var color_rect = $ColorRect
+@onready var anim_player = $AnimationPlayer
+
+func _ready():
+	color_rect.visible = false
+	anim_player.animation_finished.connect(_on_animation_finished)
+	
+func _on_animation_finished(anim_name):
+	if anim_name == "fade_to_black":
+		on_transition_finished.emit()
+		anim_player.play("fade_to_normal")
+	elif anim_name == "fade_to_normal":
+		color_rect.visible = false
+		
+	
+func transition():
+	color_rect.visible = true
+	anim_player.play("fade_to_black")
+	
