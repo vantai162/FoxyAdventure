262948656019a09d1bee47bb03825e4b1f5e0846
Hoàng Văn Tài <hoangvantai162@gmail.com>
2025-11-19 21:53:32 +0700

    okokokokoko

--- scenes/game_screen/main_menu.gd
+++ scenes/game_screen/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://test/levels/level_1/stage_1.tscn")
+	get_tree().change_scene_to_file("res://test/levels/level_1.tscn")
 
 
 func _on_options_button_pressed() -> void:
