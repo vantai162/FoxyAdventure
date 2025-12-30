e3efa6857a4081739febd7b459da8b09101a4fd4
Hoàng Văn Tài <hoangvantai162@gmail.com>
2025-12-30 22:34:48 +0700

    shop vendor now have label, adjust lv 3.4,1.1,2.1. change default input map for attack, throw

--- objects/npc/shop/shop_item/shop_vendor.gd
+++ objects/npc/shop/shop_item/shop_vendor.gd
@@ -3,6 +3,20 @@ extends Area2D
 var is_opened: bool = false
 var is_collision_with_player: bool = false
 
+@export var text: String = "Shop (Nhấn [Q] để tương tác)"
+
+@onready var text_panel: Panel = $Panel  # Lấy node Panel
+@onready var label: Label = $Panel/Label # Lấy Label con của Panel
+
+var active_tween: Tween # Biến để lưu tween đang chạy
+
+func _ready():
+	label.text = text
+	
+	# Ẩn panel đi khi bắt đầu (và làm nó trong suốt)
+	text_panel.visible = false
+	text_panel.modulate.a = 0.0
+
 @onready var animated_sprite = $Sprite2D
 @onready var shop_ui_scene: PackedScene = preload("res://objects/npc/shop/shop_ui.tscn")
 var shop_ui_instance: Control
@@ -17,6 +31,14 @@ func _physics_process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		is_collision_with_player = true
+		if active_tween:
+			active_tween.kill()
+		
+		# Tạo tween mới để fade-in
+		active_tween = create_tween()
+		text_panel.visible = true # Hiện panel
+		# Cho nó mờ dần từ 0.0 -> 1.0 trong 0.3 giây
+		active_tween.tween_property(text_panel, "modulate:a", 1.0, 0.3)
 		
 		
 
@@ -24,7 +46,17 @@ func _on_body_exited(body: Node2D) -> void:
 	if body is Player:
 		is_collision_with_player = false
 		_close_shop()
+		# Hủy tween cũ (nếu có)
+		if active_tween:
+			active_tween.kill()
+
+		# Tạo tween mới để fade-out
+		active_tween = create_tween()
+		# Mờ dần từ 1.0 -> 0.0 trong 0.3 giây
+		active_tween.tween_property(text_panel, "modulate:a", 0.0, 0.3)
 		
+		# Sau khi mờ xong, thì mới ẩn đi (ĐÃ SỬA)
+		active_tween.tween_callback(text_panel.set.bind("visible", false))
 
 # ---------------------------------------------------
 
